8eedce0c7461773ff28c2052d66c8285a80dce03
Bryan Bibat <bry@bryanbibat.net>
2014-05-21 03:12:27 +0800

    First alpha

--- app.rb
+++ app.rb
@@ -3,10 +3,12 @@ Bundler.require
 require 'sinatra/asset_pipeline'
 
 class App < Sinatra::Base
-  set :assets_host, 'bryanbibat.net'
+  set :assets_precompile, %w(app.js app.css *.png *.jpg *.svg *.eot *.ttf *.woff)
+  set :assets_host, 'datenshizero.github.io'
   set :assets_css_compressor, :sass
   set :assets_js_compressor, :uglifier
 
+  Sprockets::Helpers.prefix = "/gunner/assets"
   register Sinatra::AssetPipeline
 
   get '/' do
