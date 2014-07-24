d1117c506938cc08a4f2e52940543a7916c0f0d3
Bryan Bibat <bry@bryanbibat.net>
2014-07-25 01:56:11 +0800

    New assets

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ Bundler.require
 require 'sinatra/asset_pipeline'
 
 class App < Sinatra::Base
-  set :assets_precompile, %w(app.js app.css *.png *.jpg *.svg *.eot *.ttf *.woff *.wav *.json)
+  set :assets_precompile, %w(app.js app.css *.png *.jpg *.svg *.eot *.ttf *.woff *.ogg *.mp3 *.json)
   set :assets_host, 'datenshizero.github.io'
   set :assets_css_compressor, :sass
   set :assets_js_compressor, :uglifier
