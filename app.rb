738854fbbd09b5c03a267e3f0de1a488d87964df
Bryan Bibat <bry@bryanbibat.net>
2014-08-01 16:52:19 +0800

    Force load font

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ Bundler.require
 require 'sinatra/asset_pipeline'
 
 class App < Sinatra::Base
-  set :assets_precompile, %w(app.js app.css *.png *.jpg *.svg *.eot *.ttf *.woff *.ogg *.mp3 *.json *.xml)
+  set :assets_precompile, %w(app.js app.css *.png *.jpg *.svg *.eot *.ttf *.woff *.ogg *.mp3 *.json *.xml *.ico)
   set :assets_host, 'apps.bryanbibat.net'
   set :assets_css_compressor, :sass
   set :assets_js_compressor, :uglifier
