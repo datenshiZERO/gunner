1c5cf0a4827e47df6f7777e10331cb9fbea633d2
Bryan Bibat <bry@bryanbibat.net>
2014-08-01 15:15:54 +0800

    2nd try to tweak examples for blog

--- app.rb
+++ app.rb
@@ -3,9 +3,11 @@ Bundler.require
 require 'sinatra/asset_pipeline'
 
 class App < Sinatra::Base
-  set :assets_host, 'bryanbibat.net'
+  set :assets_precompile, %w(app.js app.css *.png *.jpg *.svg *.eot *.ttf *.woff *.ogg *.mp3 *.json *.xml)
+  set :assets_host, 'apps.bryanbibat.net'
   set :assets_css_compressor, :sass
   set :assets_js_compressor, :uglifier
+  Sprockets::Helpers.prefix = "/gunner/assets"
 
   register Sinatra::AssetPipeline
 
